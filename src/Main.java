7b80c1ca608fff2b5cfc3418de1537edfc05996e
Joanna <joannabaldyga1994@gmail.com>
2021-11-07 12:58:19 +0100

    Hello word is my first program.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        int liczba = 5;
+        int liczba2 = 2;
+        System.out.println("Hello word" + liczba);
+
+
+        System.out.println(liczba % liczba2);
+
+        String name = " Asia";
+        int age = 27;
+        System.out.println("My name is" + name + " and I have " + age + " years old.");
+
+
     }
 }
